b98a326250ff36e757bcea98f8fff2862fcad52a
steve <stevekitkat@gmail.com>
2020-07-20 08:24:22 +0700

    initial commit

---
+++ buildSrc/src/main/java/me/androidbox/buildsrc/Dependencies.kt
@@ -0,0 +1,58 @@
+package me.androidbox.buildsrc
+
+object Plugins {
+    const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlinVersion}"
+    const val androidGralePlugin = "com.android.tools.build:gradle:${Versions.gradleVersion}"
+}
+
+object Libraries {
+    const val kotlinStdlib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlinVersion}"
+    const val appCompat = "androidx.appcompat:appcompat:${Versions.androidXVersion}"
+    const val gson = "com.google.code.gson:gson:${Versions.gsonVersion}"
+    const val converterGson = "com.squareup.retrofit2:converter-gson:${Versions.converterGsonVersion}"
+    const val retrofit2 = "com.squareup.retrofit2:retrofit:${Versions.retrofitVersion}"
+    const val adapterRxjava2 = "com.squareup.retrofit2:adapter-rxjava2:${Versions.retrofitVersion}"
+    const val daggerAndroid = "com.google.dagger:dagger-android:${Versions.daggerAndroidVersion}"
+    const val daggerCompiler = "com.google.dagger:dagger-compiler:${Versions.daggerAndroidVersion}"
+    const val daggerProcessor = "com.google.dagger:dagger-android-processor:${Versions.daggerAndroidVersion}"
+    const val daggerSupport = "com.google.dagger:dagger-android-support:${Versions.daggerAndroidVersion}"
+    const val okhttp = "com.squareup.okhttp3:okhttp:${Versions.okhttpVersion}"
+    const val loggingInterceptor = "com.squareup.okhttp3:logging-interceptor:${Versions.okhttpVersion}"
+    const val rxjava = "io.reactivex.rxjava2:rxjava:${Versions.rxjavaVersion}"
+    const val rxAndroid = "io.reactivex.rxjava2:rxandroid:${Versions.rxAndroidVersion}"
+    const val rxkotlin = "io.reactivex.rxjava2:rxkotlin:${Versions.rxkotlinVersion}"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayoutVersion}"
+    const val recyclerView = "androidx.recyclerview:recyclerview:${Versions.recyclerViewVersion}"
+    const val paraceler = "org.parceler:parceler:${Versions.parcelerVersion}"
+    const val paracelerApi = "org.parceler:parceler-api:${Versions.parcelerVersion}"
+    const val supportTransition = "com.android.support:transition:${Versions.androidLibraryVersion}"
+    const val lifecycleExtensions = "androidx.lifecycle:lifecycle-extensions:${Versions.lifecycleVersion}"
+    const val firebaseCrashlytics = "com.google.firebase:firebase-crashlytics:${Versions.firebaseCrashlyticsVersion}"
+    const val roomRuntime = "androidx.room:room-runtime:${Versions.roomVersion}"
+    const val roomRxJava = "androidx.room:room-rxjava2:${Versions.roomVersion}"
+    const val roomCompiler = "androidx.room:room-compiler:${Versions.roomVersion}"
+    const val timber = "com.jakewharton.timber:timber:${Versions.timberVersion}"
+}
+
+object TestLibraries {
+    const val junit = "junit:junit:${Versions.jUnitVersion}"
+    const val assertJ = "org.assertj:assertj-core:${Versions.assertJVersion}"
+    const val mockitoKotlin = "com.nhaarman.mockitokotlin2:mockito-kotlin:${Versions.mockitoKotlinVersion}"
+    const val mockitoAndroid = "org.mockito:mockito-android:${Versions.mockitoAndroidVersion}"
+    const val runner = "androidx.test:runner:${Versions.runnerVersion}"
+    const val espressoCore = "androidx.test.espresso:espresso-core:${Versions.espressoCoreVersion}"
+    const val espressoIntents = "androidx.test.espresso:espresso-intents:${Versions.espressoCoreVersion}"
+    const val mockWebServer = "com.squareup.okhttp3:mockwebserver:${Versions.mockWebServerVersion}"
+    const val robolectric = "org.robolectric:robolectric:${Versions.robolectricVersion}"
+    const val androidxJunit = "androidx.test.ext:junit:${Versions.androidxJunitVersion}"
+    const val androidxTruth = "androidx.test.ext:truth:${Versions.androidxTruthVersion}"
+    const val androidxCore = "androidx.test:core:${Versions.androidxCoreVersion}"
+    const val androidxRules = "androidx.test:rules:${Versions.androidxRulesVersion}"
+    const val espressoContrib = "com.android.support.test.espresso:espresso-contrib:${Versions.espressoContribVersion}"
+    const val okhttp3IdlingResource = "com.jakewharton.espresso:okhttp3-idling-resource:${Versions.okhttp3IdlingResourceVersion}"
+    const val idlingConcurrent = "com.android.support.test.espresso.idling:idling-concurrent:${Versions.idlingConcurrentVersion}"
+    const val espressoIdlingResource = "androidx.test.espresso:espresso-idling-resource:${Versions.espressoIdlingResourceVersion}"
+    const val fragmentTesting = "androidx.fragment:fragment-testing:${Versions.fragmentTestingVersion}"
+    const val kakao = "com.agoda.kakao:kakao:${Versions.kakaoVersion}"
+    const val coreTesting = "androidx.arch.core:core-testing:${Versions.coreTestingVersion}"
+}
---
+++ buildSrc/src/main/java/me/androidbox/buildsrc/Versions.kt
@@ -0,0 +1,52 @@
+package me.androidbox.buildsrc
+
+object Versions {
+    /** Android */
+    const val buildToolsVersion = "28.0.3"
+    const val androidLibraryVersion = "28.0.0"
+    const val minSdkVersion = 16
+    const val targetSdkVersion = 27
+    const val compileSdkVersion = 28
+    const val versionCode = 1
+    const val versionName = "1.0"
+    const val kotlinVersion = "1.3.61"
+    const val gradleVersion = "3.4.1"
+    const val androidXVersion = "1.0.2"
+    const val gsonVersion = "2.8.5"
+    const val retrofitVersion = "2.5.0"
+    const val converterGsonVersion = retrofitVersion
+    const val daggerAndroidVersion = "2.21"
+    const val okhttpVersion = "3.12.1"
+    const val rxjavaVersion = "2.2.4"
+    const val rxAndroidVersion = "2.1.1"
+    const val rxkotlinVersion = "2.3.0"
+    const val constraintLayoutVersion = "1.1.3"
+    const val recyclerViewVersion = "1.0.0"
+    const val parcelerVersion = "1.1.12"
+    const val lifecycleVersion = "2.1.0"
+    const val firebaseCrashlyticsGradleVersion = "2.0.0-beta04"
+    const val firebaseCrashlyticsVersion = "17.0.0-beta04"
+    const val roomVersion = "2.2.5"
+    const val timberVersion = "4.7.1"
+
+    /** Testing */
+    const val jUnitVersion = "4.12"
+    const val assertJVersion = "3.11.1"
+    const val mockitoKotlinVersion = "2.1.0"
+    const val robolectricVersion = "4.3"
+    const val mockitoAndroidVersion = "2.27.0"
+    const val runnerVersion = "1.1.1"
+    const val espressoCoreVersion = "3.1.1"
+    const val espressoContribVersion = "2.0.0"
+    const val mockWebServerVersion = "3.14.0"
+    const val androidxJunitVersion = "1.0.0"
+    const val androidxTruthVersion = "1.0.0"
+    const val androidxCoreVersion = "1.1.0"
+    const val androidxRulesVersion = "1.1.0"
+    const val okhttp3IdlingResourceVersion = "1.0.0"
+    const val fragmentTestingVersion = "1.1.0"
+    const val idlingConcurrentVersion = "3.1.0"
+    const val espressoIdlingResourceVersion = "3.1.1"
+    const val kakaoVersion = "2.3.0"
+    const val coreTestingVersion = "2.1.0"
+}
