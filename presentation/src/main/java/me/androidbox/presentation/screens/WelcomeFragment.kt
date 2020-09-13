19014a6714d2beef6f87b0570536ba218c30b54e
steve <stevekitkat@gmail.com>
2020-09-13 14:14:15 +0700

    Adds custom feature map badge

--- presentation/src/main/java/me/androidbox/presentation/screens/WelcomeFragment.kt
+++ presentation/src/main/java/me/androidbox/presentation/screens/WelcomeFragment.kt
@@ -5,15 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavDirections
+import androidx.navigation.fragment.findNavController
 import me.androidbox.presentation.R
+import me.androidbox.presentation.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
+    private lateinit var binding: FragmentWelcomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+
+        binding.btnCondition.setOnClickListener {
+            findNavController().navigate(R.id.action_welcomeFragment_to_homeFragment)
+        }
+
+        return binding.root
     }
 }
---
+++ presentation/src/main/java/me/androidbox/presentation/views/FeatureMapView.kt
@@ -0,0 +1,87 @@
+package me.androidbox.presentation.views
+
+import android.content.Context
+import android.content.res.TypedArray
+import android.graphics.drawable.GradientDrawable
+import android.util.AttributeSet
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
+import me.androidbox.presentation.R
+
+class FeatureMapView @JvmOverloads constructor(context: Context, attributeSet: AttributeSet? = null, defStyleAttr: Int = 0)
+    : ConstraintLayout(context, attributeSet, defStyleAttr) {
+
+    private lateinit var tvFeatureName: TextView
+    private lateinit var ivFeatureIcon: ImageView
+    private lateinit var flBackground: FrameLayout
+
+    init {
+        if(!isInEditMode) {
+            val view = View.inflate(context, R.layout.feature_map_view, this)
+            tvFeatureName = view.findViewById(R.id.tvFeatureName)
+            ivFeatureIcon = view.findViewById(R.id.ivFeatureIcon)
+            flBackground = view.findViewById(R.id.flBackground)
+
+            background = ContextCompat.getDrawable(context, R.drawable.condition_bg)
+            val attributes = context.obtainStyledAttributes(attributeSet, R.styleable.FeatureMapView)
+
+            setBackground(attributes)
+            setupCornerRadius(attributes)
+            setFeatureName(attributes)
+            setFeatureIcon(attributes)
+            attributes.recycle()
+        }
+    }
+
+    private fun setBackground(styleAttributes: TypedArray) {
+        flBackground.background = ContextCompat.getDrawable(context, R.drawable.condition_bg)
+    }
+
+    private fun setFeatureIcon(styleAttributes: TypedArray) {
+        ivFeatureIcon.setImageResource(styleAttributes.getResourceId(R.styleable.FeatureMapView_featureIcon, R.drawable.ic_logo))
+    }
+
+    private fun setFeatureName(styleAttributes: TypedArray) {
+        tvFeatureName.text = styleAttributes.getString(R.styleable.FeatureMapView_featureName)
+        tvFeatureName.background = styleAttributes.getDrawable(R.styleable.FeatureMapView_featureNameBackground)
+    }
+
+    private fun setupCornerRadius(styleAttributes: TypedArray) {
+        val backgroundDrawable = flBackground.background.mutate()
+
+        if(backgroundDrawable is GradientDrawable) {
+            val topStartRadius = styleAttributes.getDimensionPixelSize(
+                R.styleable.FeatureMapView_topStartRadius, 0).toFloat()
+            val topEndRadius = styleAttributes.getDimensionPixelSize(
+                R.styleable.FeatureMapView_topEndRadius, 0).toFloat()
+            val bottomStartRadius = styleAttributes.getDimensionPixelSize(
+                R.styleable.FeatureMapView_bottomStartRadius, 0).toFloat()
+            val bottomEndRadius = styleAttributes.getDimensionPixelSize(
+                R.styleable.FeatureMapView_bottomEndRadius, 0).toFloat()
+            val radius = styleAttributes.getDimensionPixelSize(
+                R.styleable.FeatureMapView_featureRadius, 0).toFloat()
+
+            if(radius != 0F) {
+                backgroundDrawable.cornerRadius = radius
+            }
+            else {
+                val radiusFloatArray = floatArrayOf(
+                    topStartRadius,
+                    topStartRadius,
+                    topEndRadius,
+                    topEndRadius,
+                    bottomStartRadius,
+                    bottomStartRadius,
+                    bottomEndRadius,
+                    bottomEndRadius)
+
+                backgroundDrawable.cornerRadii = radiusFloatArray
+            }
+        }
+    }
+}
